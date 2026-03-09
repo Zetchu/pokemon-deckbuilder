a5068e585f1d336786fd37512efca8294d7a8c55
Zetchu <shmungula@gmail.com>
2026-03-09 20:23:45 +0100

    feat: update routing to use HashRouter and add GitHub Actions workflow for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import DeckBuilderPage from './pages/DeckBuilderPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path="builder" element={<DeckBuilderPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
